f1b3355fe4521ed7578fd48cc7c09943625756a9
BurukTewelde26 <74579356+BurukTewelde26@users.noreply.github.com>
2020-11-18 20:02:52 -0800

    Create JDialog_AllAccountsReport.java

---
+++ src/main/java/bank/JDialog_AllAccountsReport.java
@@ -0,0 +1,58 @@
+package bank;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.swing.JFrame; 
+import javax.swing.JScrollPane; 
+import javax.swing.JTable; 
+import javax.swing.table.DefaultTableModel;
+
+import controller.AccountService;
+import model.Account;
+import model.AccountEntry;
+ 
+public class JDialog_AllAccountsReport { 
+
+    JFrame f; 
+    
+    private AccountService accountService;
+    JDialog_AllAccountsReport(AccountService accountService) 
+    { 
+    
+    	this.accountService = accountService;
+        f = new JFrame(); 
+  
+        
+        f.setTitle("Account Report"); 
+  
+        
+         JTable account_Table = new JTable();
+        Collection<Account> accounts = new ArrayList<>();
+        accounts = accountService.getAllAccounts();
+        
+        Object[] columns = {"Acc Number", "Customer Name", "AccountType","Balance"};
+        DefaultTableModel model = new DefaultTableModel(new Object[0][0], columns);
+            
+              for(Account acc : accounts) {
+            	  model.addRow(new String[] {
+            			  acc.getAccountNumber(),
+            			  acc.getCustomer().getName(),
+            	          acc.getAccountType().toString(),
+            	          String.valueOf(acc.getBalance())});
+            	    
+              }
+              
+             
+              
+            
+        account_Table.setModel(model);
+     
+        account_Table.setBounds(30, 40, 200, 300); 
+  
+        JScrollPane sp = new JScrollPane(account_Table); 
+        f.add(sp);  
+        f.setSize(500, 200);  
+        f.setVisible(true); 
+    } 
+}
